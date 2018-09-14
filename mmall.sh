d907513e55bd02885a5fac767b5ac89170fcca1b
L <1053801783@qq.com>
2018-09-14 17:16:55 +0800

    更新了脚本文件

--- mmall.sh
+++ mmall.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+!/bin/bash
 
 #mmall shell 
 #entrance
 entrance=/root/L/developer/mmall
 #out
-out=/root/L/project/mmall
+out=/root/L/project/mmall/dist
 
 #cd dir
 echo -e "\033[36m ==================cd entrance================== \033[0m"
@@ -13,32 +13,30 @@ cd $entrance
 #clear dist
 echo -e "\033[36m ==================clear git==================== \033[0m"
 if [ -d "./dist" ]
-	then
-	rm -rf dist
+        then
+        rm -rf dist
 else
-	echo -e  "\033[36m =========dist not found \033[0m"
+        echo -e  "\033[36m =========dist not found \033[0m"
 fi
 
 #git pull
 echo -e "\033[36m =========git pull========== \033[0m"
 git pull git@github.com:AboyL/mmall-fe.git
 
-echo -e "\033[36m ========= cnpm install ========== \033[0m"
+echo -e "\033[36m ========= yarn ========== \033[0m"
 
-#npm install
-cnpm install
+# install
+yarn
 
-echo -e "\033[36m =========npm run ========== \033[0m"
+echo -e "\033[36m ========= yarn dist ========== \033[0m"
 #run 
-npm run dist
+yarn dist
 
 #if dist is a dir mv it to out
 if [ -d "./dist" ]
-	then
-	echo "\033[36m ===========================dist is dir=================== \033[0m"
+        then
+        echo "\033[36m ===========================dist is dir=================== \033[0m"
 #mv dist
-	echo "\033[36m ==================cp dist to out============= \033[0m"
-	cp -R ./dist $out
+        echo "\033[36m ==================cp dist to out============= \033[0m"
+        cp -R ./dist $out
 fi
-
-
